b12d021fc11581dc74bc303f74655fd0fc34b382
Aikanush <haka.m@mail.ru>
2020-01-14 21:00:08 +0400

    changed task 1

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -2,10 +2,11 @@ const showTimer = function() {
 	const timer = document.getElementById('timer');
 	timer.textContent -= 1;
 	if (timer.textContent == 0) {
+		clearInterval(timers);
 		alert(`«Вы победили в конкурсе!»`);
 	}
   
     
 }
 
-setInterval(showTimer, 1000);
+let timers = setInterval(showTimer, 1000);
