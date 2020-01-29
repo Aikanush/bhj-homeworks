542f568c547b6e6a7876ede04445874f4ed0b69f
Aikanush <haka.m@mail.ru>
2020-01-29 18:23:57 +0400

    task 2

---
+++ element-search/menu/task.js
@@ -0,0 +1,27 @@
+const menu = document.querySelector('.menu_main');
+
+// const menuSub = document.querySelectorAll('.menu_sub');
+
+
+
+menu.addEventListener('click', function(event) {
+  let target = event.target;
+
+  let targetParent = target.closest('.menu__item');
+
+  if (targetParent) {
+    let subm = targetParent.getElementsByClassName('menu_sub')[0];
+    close();
+    if (subm) {
+      subm.style.display = 'block';
+      event.preventDefault();
+    }
+  }
+});
+
+function close() {
+  let s = document.getElementsByClassName('menu_sub');
+  for (let i = 0; i < s.length; i++) {
+    s[i].style.display = 'none';
+  }
+}
--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -19,3 +19,41 @@ firstElement.onclick = () => {
 secondElement.onclick = () => {
 	modal_showed[1].classList.remove('modal_active');
 }
+
+
+// menu.onclick = function(event) {
+// 	let target = event.target;
+// 	if (target.className == 'menu__item') {
+// 		let sub = target.getElementsByClassName('menu_sub');
+// 		closeMenu();
+// 		sub[0].style.displey = 'block';
+
+// 	}
+// }
+
+// document.onclick = function(event) { 
+// 	let target = event.target;
+// 	console.log(event.target);
+// 	if (target.className != 'menu__item' && target.className != 'menu_sub') {
+// 		closeMenu();
+// 	}
+// }
+
+// function closeMenu() {
+// 	for (let i = 0; i < menuSub.length; i++) {
+// 		menuSub[i].style.display = 'none';
+// 	}
+// }
+ 
+	
+// for (let i = 0; i < link.length; i++) {
+// 	link.onclick = () => {
+//       if (link[i].classList.contains('menu_sub')) {
+
+//       }
+// 	}
+// }
+
+
+// const main = document.getElementsByClassName('menu_main');
+
