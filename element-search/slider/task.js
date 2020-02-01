a9f8951a9881b3e4dae67f095c2a8f35e9ff9a6a
Aikanush <haka.m@mail.ru>
2020-02-01 18:29:15 +0400

    task 3

---
+++ element-search/slider/task.js
@@ -0,0 +1,85 @@
+
+const body = document.body;
+const items = document.querySelectorAll('.slider__item');
+const dots = document.querySelectorAll('.slider__dots')
+const dot = document.querySelectorAll('.slider__dot')
+let currentSlide = 0;
+
+body.addEventListener('click', event => {
+  const target = event.target;
+
+  if(target.classList.contains('slider__arrow_prev')) {
+    for(let value of items) {
+      if(value.classList.contains('slider__item_active')) {
+        value.classList.remove('slider__item_active');
+      }
+    }
+
+    for(let value of dot) {
+ 	  if(value.classList.contains('slider__dot_active')){
+ 		value.classList.remove('slider__dot_active')
+ 	  }
+ 	}
+
+    if(currentSlide < 0) {
+      currentSlide = items.length - 1;
+      items[currentSlide].classList.add('slider__item_active');
+      dot[currentSlide].classList.add('slider__dot_active');
+      currentSlide -= 1
+      
+    }
+    items[currentSlide].classList.add('slider__item_active');
+    dot[currentSlide].classList.add('slider__dot_active');
+      currentSlide -= 1;
+
+  }
+
+  if (target.classList.contains('slider__arrow_next')) {
+  	for(let value of items) {
+  		if(value.classList.contains('slider__item_active')) {
+  			value.classList.remove('slider__item_active');
+  		}
+  	}
+
+  	 for(let value of dot) {
+ 	  if(value.classList.contains('slider__dot_active')){
+ 		value.classList.remove('slider__dot_active')
+ 	  }
+ 	}
+
+  	if(currentSlide >= items.length) {
+  		currentSlide = 0;
+  		items[currentSlide].classList.add('slider__item_active');
+  		dot[currentSlide].classList.add('slider__dot_active');
+  		currentSlide += 1;
+  	}
+  	items[currentSlide].classList.add('slider__item_active');
+  	dot[currentSlide].classList.add('slider__dot_active');
+       currentSlide += 1;
+  }
+
+})
+
+for(let i = 0; i < dots[0].children.length; i++) {
+	dots[0].children[i].addEventListener('click', function() {
+		currentSlide = i;
+		
+ 	    if (event.target) {
+ 	    	for(let value of items) {
+              if(value.classList.contains('slider__item_active')) {
+              value.classList.remove('slider__item_active');
+              }
+           }
+
+           for(let value of dot) {
+ 	          if(value.classList.contains('slider__dot_active')){
+ 		      value.classList.remove('slider__dot_active')
+ 	          }
+ 	       }
+		   items[currentSlide].classList.add('slider__item_active');
+           dot[currentSlide].classList.add('slider__dot_active');
+       }
+	})
+}
+
+
